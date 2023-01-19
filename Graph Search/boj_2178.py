86f25344487fe03b789d6d960bb800d93321bc59
Lee hh <ynwa2022@naver.com>
2023-01-19 17:53:55 +0900

    Study Graph Search

---
+++ Graph Search/boj_2178.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+def bfs(x,y):
+    queue = deque()
+    queue.append((x,y))
+    while queue:
+        x,y = queue.popleft()
+        for i in range(4):
+            nx = x+dx[i]
+            ny = y +dy[i]
+            if nx <0 or nx >= n or ny <0 or ny >= m:
+                continue
+            if graph[nx][ny] == 0:
+                continue
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] +1
+                queue.append((nx,ny))
+    return graph[n-1][m-1]
+
+
+n,m = map(int,input().split())
+graph = []
+
+for _ in range(n):
+    graph.append(list(map(int,input())))
+
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+
+print(bfs(0,0))
+
+#미로탈출 문제와 같이 간선비용이 모두 같고,최단 거리를 탐색할 시에 bfs알고리즘의 사용이 적절하다. 
